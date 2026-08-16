037653bf607fae5a9b0e2f29010bb585d3943038
Dávid Szakállas <5807322+dszakallas@users.noreply.github.com>
2026-08-16 12:30:08 +0200

    modules/darwin: export PRISMA_CLI_BINARY in litellm startScript to prevent nodeenv lookup

--- modules/darwin/litellm.nix
+++ modules/darwin/litellm.nix
@@ -87,6 +87,7 @@ let
       fi
 
       export PYTHONPATH="${cfg.stateDir}/site-packages:''${PYTHONPATH:-}"
+      export PRISMA_CLI_BINARY="${pkgs.prisma_6}/bin/prisma"
       export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines_6}/bin/query-engine"
       export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines_6}/bin/schema-engine"
       export PRISMA_FMT_BINARY="${pkgs.prisma-engines_6}/bin/prisma-fmt"
